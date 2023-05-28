5d1cdd3f262fa7d8c2ff599a2cce6d3092b484c5
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-05-28 06:36:38 +0600

    full commit

---
+++ src/pages/Signup/Signup.jsx
@@ -0,0 +1,74 @@
+import { Link } from 'react-router-dom';
+import img from '../../assets/images/login/login.svg'
+import { useContext } from 'react';
+import { AuthContext } from '../../providers/AuthProviders';
+
+const Signup = () => {
+    const {createUser} = useContext(AuthContext);
+
+    const handleSingUp = event => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.pass.value;
+        console.log(name, email, password);
+
+        createUser(email, password)
+        .then(result =>{
+            const user = result.user;
+            console.log(user);
+        })
+        .catch(error => console.log(error));
+    }
+
+    return (
+        <div>
+            <div className="hero min-h-screen bg-base-200">
+                <div className="hero-content flex-col lg:flex-row">
+                    <div className="w-1/2 mr-12">
+                        <img src={img} alt="Sign Up Image" />
+                    </div>
+                    <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                        <div className="card-body">
+                            <h1 className="text-3xl text-center font-bold">Sign Up</h1>
+                            <form onSubmit={handleSingUp}>
+                                {/* name field */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Name</span>
+                                    </label>
+                                    <input type="text" name='name' placeholder="Your Name" className="input input-bordered" />
+                                </div>
+                                {/* email field */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Email</span>
+                                    </label>
+                                    <input type="email" name='email' placeholder="email" className="input input-bordered" />
+                                </div>
+                                {/* password field */}
+                                <div className="form-control">
+                                    <label className="label">
+                                        <span className="label-text">Password</span>
+                                    </label>
+                                    <input type="password" name='pass' placeholder="password" className="input input-bordered" />
+                                    <label className="label">
+                                        <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                    </label>
+                                </div>
+                                {/* input submit */}
+                                <div className="form-control mt-6">
+                                    <input className="btn btn-primary" type="submit" value="Sign Up" />
+                                </div>
+                            </form>
+                            <p className=' my-4 text-center'> Already Have An Account? <Link className='text-blue-400 font-semibold' to='/login'>Please Login</Link></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Signup;
